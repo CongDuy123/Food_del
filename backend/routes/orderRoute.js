ae563fcce9ccc270a021686ae7f5f3bc133b57d0
CongDuy123 <129979353+CongDuy123@users.noreply.github.com>
2025-07-30 17:01:59 +0700

    Update orderRoute.js

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,6 +1,6 @@
 import express from "express"
 import authMiddleware from "../middleware/auth.js"
-import { placeOrder, userOrders, verifyOrder, ListOrders } from "../controllers/orderController.js"
+import { placeOrder, userOrders, verifyOrder, ListOrders, updateStatus } from "../controllers/orderController.js"
 
 const orderRouter = express.Router();
 
@@ -8,5 +8,7 @@ orderRouter.post("/place", authMiddleware, placeOrder)
 orderRouter.post("/verify", verifyOrder)
 orderRouter.post("/userOrders", authMiddleware, userOrders)
 orderRouter.get("/list", ListOrders)
+orderRouter.post("/status", updateStatus)
+
 
 export default orderRouter;
