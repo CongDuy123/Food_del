591997d92a547a1e3e5c2c9de115021722e9be3a
CongDuy123 <129979353+CongDuy123@users.noreply.github.com>
2025-07-30 16:49:23 +0700

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -8,7 +8,7 @@ import Orders from "./pages/Orders/Orders";
 import { ToastContainer } from "react-toastify";
 
 const App = () => {
-  const url = "http://localhost:4000";
+  const url = "https://food-del-backend-wudr.onrender.com";
 
   return (
     <div>
