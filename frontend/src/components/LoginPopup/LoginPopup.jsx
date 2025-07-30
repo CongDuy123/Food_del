1a0db4b806fb46102e49e54d3c1e98cd8f8e50c0
CongDuy123 <129979353+CongDuy123@users.noreply.github.com>
2025-07-30 16:30:51 +0700

    Update LoginPopup.jsx

--- frontend/src/components/LoginPopup/LoginPopup.jsx
+++ frontend/src/components/LoginPopup/LoginPopup.jsx
@@ -34,6 +34,7 @@ const LoginPopup = ({ setShowLogin }) => {
     if (response.data.success) {
       setToken(response.data.token);
       localStorage.setItem("token", response.data.token);
+      setShowLogin(false);
     } else {
       alert(response.data.message);
     }
